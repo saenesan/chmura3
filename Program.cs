a238130570f57b6177bd0435be1f1a651a1d7c76
saenesan <116022171+saenesan@users.noreply.github.com>
2025-06-06 18:15:45 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,19 +1,21 @@
+using BestTeamAppFull.Services;
 using BestTeamAppFull.Data;
 using Microsoft.EntityFrameworkCore;
-using BestTeamApp.Services; // ← upewnij się, że to jest
-
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+
+// ⭐ Rejestracja DbContext i TeamService
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
 builder.Services.AddScoped<TeamService>();
 
 var app = builder.Build();
 
 app.UseStaticFiles();
 app.UseRouting();
+
 app.MapDefaultControllerRoute();
+
 app.Run();
